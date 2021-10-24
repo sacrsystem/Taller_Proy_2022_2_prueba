1ca8f73573a30ab1f2ce97b8a56ba0c6ba5bc5d9
Julio Ucharima <julio_ucharima@usmp.pe>
2021-10-24 00:30:16 -0500

    funcionalidad back to top button

---
+++ js/SC_IBM/bttopbutton.js
@@ -0,0 +1,29 @@
+// Scroll up
+
+document.getElementById("button-up").addEventListener("click", scrollUp);
+
+function scrollUp(){
+
+    var currentScroll = document.documentElement.scrollTop;
+
+    if (currentScroll > 0){
+        window.scrollTo (0, 5);
+    }
+}
+
+
+///
+
+buttonUp = document.getElementById("button-up");
+
+window.onscroll = function(){
+
+    var scroll = document.documentElement.scrollTop;
+
+    if (scroll > 200){
+        buttonUp.style.transform = "scale(1)";
+    }else if(scroll < 200){
+        buttonUp.style.transform = "scale(0)";
+    }
+
+}
